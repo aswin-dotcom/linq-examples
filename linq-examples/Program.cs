d9d00c0475ce6466c671bebb19ab50242808f41a
aswin-dotcom <aswin42samuel7@gmail.com>
2025-09-07 20:30:32 +0530

    Sorting

--- linq-examples/Program.cs
+++ linq-examples/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //Sorting using wordLength
+            string[] words = { "The", "Quick", "brown", "fox", "Jump" };
+            var query = from word in words
+                        orderby word.Length
+                        select word;
+            foreach (var word in query)
+            {
+                Console.WriteLine(word);
+            }
+
+            //sorting from the 1st letter
+            string[] words2 = { "The", "Quick", "brown", "fox", "Jump" };
+            var query2 = from word in words2
+                         orderby word.Substring(0, 1)
+                         select word;
+
+            foreach(var word in query2)
+            {
+                Console.WriteLine(word);
+            }
+
         }
-    }
+
+            
+    }   
 }
