aa15b31f378037441943159e819353ce4c52aa57
aswin-dotcom <aswin42samuel7@gmail.com>
2025-09-07 20:14:34 +0530

    Add project files.

---
+++ linq-examples/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace linq_examples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
