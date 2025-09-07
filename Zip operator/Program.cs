8e497ab2d3d4e9e863ff25b7f238a5f44006bdb8
aswin-dotcom <aswin42samuel7@gmail.com>
2025-09-07 21:23:22 +0530

    Zip-Operator

---
+++ Zip operator/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Zip_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list1 = { 1, 2, 3 };
+            string[] text = { "A", "B", "C" };
+            var query =  Enumerable.Zip(list1, text ,(number,letter)=>number.ToString()+letter);
+            foreach(string i in query)
+
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
