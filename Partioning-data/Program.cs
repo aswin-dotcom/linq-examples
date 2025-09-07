54f8d3f8545cc1a1a33b173c13d4cb78b42834be
aswin-dotcom <aswin42samuel7@gmail.com>
2025-09-07 21:28:29 +0530

    Partioning data

---
+++ Partioning-data/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Partioning_data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] list1 = {1, 2, 3,4,5};
+            //var query = list1.Skip(3);
+            //var query = list1.SkipWhile(i => i != 3);
+            //var query = list1.Take(3);
+            var query = list1.TakeWhile(i => i != 3);
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
